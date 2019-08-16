9cd8f360d7bc05a1c1580b1e292ae60902ee52f2
Weiner <woliveira82@gmail.com>
2019-08-15 22:20:01 -0300

    Chat working

--- run.py
+++ run.py
@@ -1,11 +1,20 @@
 from flask import Flask
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
+socketio = SocketIO(app)
+
 
 @app.route('/')
 def hello():
     return open('views/index.html').read()
 
 
+@socketio.on('message')
+def messageReceive(data):
+    print(data)
+    emit('message', data, broadcast=True)
+
+
 if __name__ == '__main__':
-    app.run('127.0.0.1', '5000', debug=True)
+    socketio.run(app, '0.0.0.0', '5000')
